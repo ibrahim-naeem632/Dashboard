12f4cfcc3c066d4d75431bc3b768f2f4e526a028
Ibrahim Naeem <157243993+ibrahim-naeem632@users.noreply.github.com>
2026-08-19 00:24:01 +0500

    Delete src directory

--- src/utils/format.ts
+++
@@ -1,7 +0,0 @@
-export const formatCurrency = (num: number) => {
-  return "$" + num.toLocaleString();
-};
-
-export const formatChange = (num: number) => {
-  return `${num > 0 ? "+" : ""}${num}%`;
-};
